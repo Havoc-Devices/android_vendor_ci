b32c1588acfbdd41660f68c6ef505e3534b4853f
Sharma Ji <66856959+revolwoc@users.noreply.github.com>
2022-07-13 08:26:22 +0530

    Create alioth.sh

---
+++ alioth.sh
@@ -0,0 +1,14 @@
+rm -rf hardware/qcom-caf/sm8250/audio hardware/qcom-caf/sm8250/display hardware/qcom-caf/sm8250/media hardware/xiaomi vendor/qcom/opensource/usb external/ant-wireless/antradio-library packages/resources/devicesettings
+
+git clone https://github.com/Havoc-Devices/alioth_device_xiaomi_alioth device/xiaomi/alioth
+git clone https://github.com/Havoc-Devices/alioth_device_xiaomi_sm8250-common device/xiaomi/sm8250-common
+git clone https://github.com/Havoc-Devices/alioth_vendor_xiaomi_alioth vendor/xiaomi/alioth
+git clone https://github.com/Havoc-Devices/alioth_vendor_xiaomi_sm8250-common vendor/xiaomi/sm8250-common
+git clone https://github.com/Havoc-Devices/alioth_kernel_xiaomi_alioth kernel/xiaomi/alioth
+git clone https://github.com/Havoc-Devices/alioth_hardware_xiaomi hardware/xiaomi
+git clone https://github.com/Havoc-Devices/alioth_vendor_qcom_opensource_usb vendor/qcom/opensource/usb
+git clone https://github.com/Havoc-Devices/alioth_external_ant-wireless_antradio-library external/ant-wireless/antradio-library
+git clone https://github.com/Havoc-Devices/alioth_hardware_qcom-caf_sm8250_display hardware/qcom-caf/sm8250/display
+git clone https://github.com/Havoc-Devices/alioth_hardware_qcom-caf_sm8250_media hardware/qcom-caf/sm8250/media
+git clone https://github.com/Havoc-Devices/alioth_hardware_qcom-caf_sm8250_audio hardware/qcom-caf/sm8250/audio
+git clone https://github.com/Havoc-Devices/alioth_packages_resources_devicesettings packages/resources/devicesettings
