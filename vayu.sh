1605d43faf627872083a8427809563d008806dcd
Sukeerat Goindi <sukeerat@goindi.org>
2022-07-17 10:45:51 +0530

    Clean default repos

--- vayu.sh
+++ vayu.sh
@@ -1,5 +1,2 @@
-git clone https://github.com/Havoc-Devices/android_device_xiaomi_vayu device/xiaomi/vayu 
-git clone https://github.com/Havoc-Devices/android_vendor_xiaomi_vayu vendor/xiaomi/vayu
-git clone https://github.com/Havoc-Devices/android_kernel_xiaomi_vayu kernel/xiaomi/vayu
-git clone https://github.com/Havoc-Devices/android_hardware_xiaomi hardware/xiaomi
+rm -rf packages/resources/devicesettings
 git clone https://github.com/Havoc-Devices/android_packages_resources_devicesettings packages/resources/devicesettings
