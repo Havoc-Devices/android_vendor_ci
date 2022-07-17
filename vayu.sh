324e299458b8db8f965d3f441161ee39bebf9c1e
Sukeerat Goindi <sukeerat@goindi.org>
2022-07-17 12:11:10 +0530

    Branches

--- vayu.sh
+++ vayu.sh
@@ -1,2 +1,2 @@
 rm -rf packages/resources/devicesettings
-git clone https://github.com/Havoc-Devices/android_packages_resources_devicesettings packages/resources/devicesettings
+git clone https://github.com/Havoc-Devices/android_packages_resources_devicesettings packages/resources/devicesettings -b twelve
