98638864922a3a95ceac1cab9319f66e27ebbc8d
Sukeerat Goindi <sukeerat@goindi.org>
2022-07-17 10:57:51 +0530

    Introduce alioth

---
+++ alioth.sh
@@ -0,0 +1,12 @@
+rm -rf  hardware/xiaomi  vendor/qcom/opensource/usb external/ant-wireless/antradio-library hardware/qcom-caf/sm8250/display hardware/qcom-caf/sm8250/audio  hardware/qcom-caf/sm8250/media packages/resources/devicesettings vendor/nxp/opensource/commonsys/packages/apps/Nfc
+   
+
+git clone https://github.com/Havoc-Devices/android_hardware_xiaomi_alioth hardware/xiaomi -b  twelve
+git clone https://github.com/Havoc-Devices/android_vendor_qcom_opensource_usb vendor/qcom/opensource/usb -b  twelve
+git clone https://github.com/Havoc-Devices/android_external_ant-wireless_antradio-library external/ant-wireless/antradio-library -b  twelve
+git clone https://github.com/Havoc-Devices/android_hardware_qcom-caf_sm8250_display hardware/qcom-caf/sm8250/display -b  twelve
+git clone https://github.com/Havoc-Devices/android_hardware_qcom-caf_sm8250_audio hardware/qcom-caf/sm8250/audio -b  twelve
+git clone https://github.com/Havoc-Devices/android_hardware_qcom-caf_sm8250_media hardware/qcom-caf/sm8250/media -b  twelve
+git clone https://github.com/Havoc-Devices/android_packages_resources_devicesettings packages/resources/devicesettings   -b  twelve
+git clone https://github.com/Havoc-Devices/android_vendor_nxp_opensource_commonsys_packages_apps_Nfc vendor/nxp/opensource/commonsys/packages/apps/Nfc -b  twelve
+
