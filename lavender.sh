c9bb10887b869f96599eeb5b749518b452025688
Vitorgl2003 <vitorgl.2003@gmail.com>
2022-08-03 01:35:11 +0530

    lavender: Set branch dependencie to eleven
    Signed-off-by: Vitorgl2003 <vitorgl.2003@gmail.com>

--- lavender.sh
+++ lavender.sh
@@ -1,11 +1,11 @@
 rm -rf hardware/qcom-caf/msm8998/media vendor/havoc/charger hardware/qcom-caf/msm8998/display hardware/qcom-caf/msm8998/audio packages/resources/devicesettings
 
-git clone git@github.com:Havoc-Devices/android_device_xiaomi_lavender device/xiaomi/lavender
-git clone https://github.com/Havoc-Devices/vendor_xiaomi_lavender vendor/xiaomi/lavender
-git clone https://github.com/Havoc-Devices/kernel_xiaomi_lavender kernel/xiaomi/lavender
-git clone https://github.com/ArrowOS/android_hardware_qcom_media -b arrow-11.0-caf-msm8998 hardware/qcom-caf/msm8998/media
-git clone https://github.com/ArrowOS/android_hardware_qcom_audio -b arrow-11.0-caf-msm8998 hardware/qcom-caf/msm8998/audio
-git clone https://github.com/ArrowOS/android_hardware_qcom_display -b arrow-11.0-caf-msm8998 hardware/qcom-caf/msm8998/display
-git clone https://github.com/Vitorgl2003/packages_resources_devicesettings packages/resources/devicesettings
+git clone git@github.com:Havoc-Devices/android_device_xiaomi_lavender -b eleven device/xiaomi/lavender
+git clone https://github.com/Havoc-Devices/vendor_xiaomi_lavender -b eleven vendor/xiaomi/lavender
+git clone https://github.com/Havoc-Devices/kernel_xiaomi_lavender -b eleven kernel/xiaomi/lavender
+git clone https://github.com/Havoc-Devices/hardware_qcom-caf_msm8998 -b media-11.0 hardware/qcom-caf/msm8998/media
+git clone https://github.com/Havoc-Devices/hardware_qcom-caf_msm8998 -b audio-11.0 hardware/qcom-caf/msm8998/audio
+git clone https://github.com/Havoc-Devices/hardware_qcom-caf_msm8998 -b display-11.0 hardware/qcom-caf/msm8998/display
+git clone https://github.com/Havoc-Devices/android_packages_resources_devicesettings -b eleven packages/resources/devicesettings
 
 export build_type='user'
