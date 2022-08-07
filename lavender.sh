bb61d69dc555bf72e569ffe25a1f0c2ee9e8fda1
Vitorgl2003 <vitorgl.2003@gmail.com>
2022-08-07 21:32:39 +0100

    lavender: Udpate .sh file
    Signed-off-by: Vitorgl2003 <vitorgl.2003@gmail.com>

--- lavender.sh
+++ lavender.sh
@@ -1,6 +1,6 @@
-rm -rf hardware/qcom-caf/msm8998/media vendor/havoc/charger hardware/qcom-caf/msm8998/display hardware/qcom-caf/msm8998/audio packages/resources/devicesettings
+rm -rf hardware/qcom-caf/msm8998/media hardware/qcom-caf/msm8998/display hardware/qcom-caf/msm8998/audio vendor/havoc/charger packages/resources/devicesettings device/xiaomi/extras
 
-git clone git@github.com:Havoc-Devices/android_device_xiaomi_lavender -b eleven device/xiaomi/lavender
+git clone git@github.com:Havoc-Devices/android_device_xiaomi_lavender.git -b eleven device/xiaomi/lavender
 git clone https://github.com/Havoc-Devices/vendor_xiaomi_lavender -b eleven vendor/xiaomi/lavender
 git clone https://github.com/Havoc-Devices/kernel_xiaomi_lavender -b eleven kernel/xiaomi/lavender
 git clone https://github.com/Havoc-Devices/hardware_qcom-caf_msm8998 -b media-11.0 hardware/qcom-caf/msm8998/media
