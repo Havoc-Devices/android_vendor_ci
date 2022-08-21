2ca0b209d87dc08829b8652f2f4f4a64d9785d88
Sukeerat Goindi <sukeerat@goindi.org>
2022-08-21 15:41:45 +0530

    Raphael Clang Derp!

--- raphael.sh
+++ raphael.sh
@@ -1,6 +1,6 @@
 rm -rf hardware/qcom-caf/sm8150/display
 
 git clone git@github.com:Havoc-Devices/hardware_qcom-caf_sm8150_display.git hardware/qcom-caf/sm8150/display -b twelve 
-git clone git@gitlab.com:havoc-devices/trb_clang -b twelve 
+git clone git@gitlab.com:havoc-devices/trb_clang -b prebuilts/clang/host/linux-x86/trb_clang twelve 
 
 export SELINUX_IGNORE_NEVERALLOWS=true
